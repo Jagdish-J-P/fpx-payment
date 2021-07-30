efa8190fa3f190c6165f7ca75243c0eb8bee1216
Jagdish-J-P <jagdish.j.ptl@gmail.com>
2021-07-30 22:56:43 +0530

    renamed transaction table name

--- database/migrations/2021_07_13_170430_create_fpx_transactions_table.php
+++ database/migrations/2021_07_13_170430_create_fpx_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTransactionsTable extends Migration
+class CreateFpxTransactionsTable extends Migration
 {
 	/**
 	 * Run the migrations.
